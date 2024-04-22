ce3a8f847995153698ba98e3e83c3a5267acb29c
Tsar <TsarVul@protonmail.com>
2024-04-22 15:30:35 +0300

    Fix bug where manually set offset is ignored

--- Source/Transport.cpp
+++ Source/Transport.cpp
@@ -2,39 +2,9 @@
 #include "Transport.h"
 
 //==============================================================================
-Transport::Transport(M1OrientationClient* orientationClient)
+Transport::Transport(M1OrientationClient* orientationClient) : m_orientation_client(orientationClient)
 {
-	HH = 0;
-	MM = 0; 
-	SS = 0;
-	FS = 0;
-
-	this->m_orientation_client = orientationClient;
-
     startTimer(50);
-}
-
-void Transport::sendData() {
-
-    if (m_orientation_client == nullptr) return;
-
-    auto play_head_position = m_correct_time_in_seconds + 1.0 * m_transport_offset / 1000;
-
-    m_orientation_client->command_setPlayerPositionInSeconds(play_head_position);
-    m_orientation_client->command_setPlayerIsPlaying(getIsPlaying());
-}
-
-void Transport::setOffset(int hh, int mm, int ss, int fs) {
-    if ((hh == HH) && (mm == MM) && (ss == SS) && (fs == FS)) {
-        return;
-    }
-
-    HH = hh;
-    MM = mm;
-    SS = ss;
-    FS = fs;
-
-    m_transport_offset = hh * 1000 * 60 * 60 + mm * 1000 * 60 + ss * 1000 + fs;
 }
 
 double Transport::getTimeInSeconds() const {
@@ -51,6 +21,17 @@ bool Transport::getIsPlaying() const {
 
 void Transport::setIsPlaying(bool is_playing) {
     m_is_playing = is_playing;
+}
+
+void Transport::sendData() {
+
+    if (m_orientation_client == nullptr) return;
+
+    m_transport_offset = HH * 1000 * 60 * 60 + MM * 1000 * 60 + SS * 1000 + FS;
+    auto play_head_position = m_correct_time_in_seconds + m_transport_offset / 1000;
+
+    m_orientation_client->command_setPlayerPositionInSeconds(play_head_position);
+    m_orientation_client->command_setPlayerIsPlaying(getIsPlaying());
 }
 
 void Transport::timerCallback() {
--- Source/Transport.h
+++ Source/Transport.h
@@ -10,22 +10,19 @@ public:
 
     Transport(M1OrientationClient* orientationClient);
 
-    void setOffset(int hh, int mm, int ss, int fs);
-
     double getTimeInSeconds() const;
 
     void setTimeInSeconds(double time_in_seconds);
 
     bool getIsPlaying() const;
 
-    void setIsPlaying( bool is_playing);
+    void setIsPlaying(bool is_playing);
 
     void sendData();
 
     void timerCallback() override;
 
 public:
-
     int HH = 0;
     int MM = 0;
     int SS = 0;
@@ -35,8 +32,6 @@ private:
 	M1OrientationClient* m_orientation_client = nullptr;
 
     double m_transport_offset = 0;
-
 	double m_correct_time_in_seconds = 0;
-
     bool m_is_playing = true;
 };
