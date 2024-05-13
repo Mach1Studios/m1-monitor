35e2f7ddf8e2881a9a018dd03505ebfb8cc6de47
Dylan Marcus <dylan@dylanjmarcus.com>
2024-05-13 17:25:27 -0400

    transport corrected for sending play updates only after the timecode offset is reached
    (cherry picked from commit 69f2259e32fbc8348a41ec37e10084abac6680d6)

--- Source/Transport.cpp
+++ Source/Transport.cpp
@@ -26,13 +26,14 @@ void Transport::setIsPlaying(bool is_playing) {
 }
 
 void Transport::sendData() {
-
     if (m_orientation_client == nullptr) return;
 
     m_transport_offset = HH * 1000 * 60 * 60 + MM * 1000 * 60 + SS * 1000 + FS;
-    auto play_head_position = m_correct_time_in_seconds + m_transport_offset / 1000;
-
-    m_orientation_client->command_setPlayerPositionInSeconds(play_head_position);
+    auto play_head_position = m_correct_time_in_seconds - m_transport_offset / 1000;
+    if (play_head_position < 0) play_head_position = 0;
+    if (play_head_position != 0) {
+        m_orientation_client->command_setPlayerPositionInSeconds(play_head_position);
+    }
     m_orientation_client->command_setPlayerIsPlaying(getIsPlaying());
 }
 
