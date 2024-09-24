15882bebb0a291caa707fe406d37582809bc58ca
Dylan Marcus <dylan@dylanjmarcus.com>
2024-09-24 14:53:09 -0400

    attempt to use ga4 instead of ua

--- Source/M1Analytics.cpp
+++ Source/M1Analytics.cpp
@@ -5,17 +5,15 @@ juce::StringPairArray M1Analytics::createUsageData(const juce::String& event)
     const auto anon_id = juce::String(juce::SystemStats::getDeviceIdentifiers().joinIntoString(":").hashCode64());
 
     juce::StringPairArray data;
-    data.set("v", "1");
-    data.set("tid", "UA-145097104-1");
-    data.set("cid", anon_id);
-    data.set("t", "event");
-    data.set("ec", "info");
+    data.set("event_name", "MonitorLaunch");
+    data.set("client_id", anon_id);
+    data.set("type", "event");
     data.set("ea", event);
 
+    data.set("user_id", anon_id);
     data.set("cd1", juce::SystemStats::getUserRegion());
     data.set("cd2", juce::SystemStats::getOperatingSystemName());
     data.set("cd3", juce::SystemStats::getDeviceDescription());
-    data.set("cd4", anon_id);
 
     juce::String appType, appName, appVersion, appManufacturer;
 
@@ -54,7 +52,6 @@ juce::StringPairArray M1Analytics::createUsageData(const juce::String& event)
 
 M1Analytics::M1Analytics(const juce::String& event) : juce::ThreadPoolJob("M1-Monitor Analytics")
 {
-    const auto address = "https://www.google-analytics.com/collect";
     auto agentCPUVendor = juce::SystemStats::getCpuVendor();
 
     if (agentCPUVendor.isEmpty())
@@ -77,7 +74,11 @@ M1Analytics::M1Analytics(const juce::String& event) : juce::ThreadPoolJob("M1-Mo
         if (parameters[key].isNotEmpty())
             postData.add(key + "=" + juce::URL::addEscapeChars(parameters[key], true));
 
-    url = juce::URL(address).withPOSTData(postData.joinIntoString("&"));
+    const auto address = "https://www.google-analytics.com/mp/collect?";
+    url = juce::URL(address)
+              .withParameter("measurement_id", apiKey)
+              .withParameter("api_secret", secret)
+              .withPOSTData(postData.joinIntoString("\r\n"));
 }
 
 juce::ThreadPoolJob::JobStatus M1Analytics::runJob()
