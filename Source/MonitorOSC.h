938b82a7a48bcd21594c704e952a6388c0b3b7ed
Dylan Marcus <dylan@dylanjmarcus.com>
2024-11-04 13:04:56 -0500

    only active monitor sends playhead info, playhead updated on timercallback, renamed transport vars

--- Source/MonitorOSC.h
+++ Source/MonitorOSC.h
@@ -7,8 +7,8 @@ class MonitorOSC : private juce::OSCSender, private juce::OSCReceiver, private j
     bool init(int helperPort);
     bool initFromSettings(std::string jsonSettingsFilePath);
     int helperPort = 0, port = 0;
-    bool isConnected = false; // used to track connection with helper utility
-    bool isActiveMonitor = true; // used to track if this is the primary monitor instance
+    bool is_connected = false; // used to track connection with helper utility
+    bool is_active_monitor = true; // used to track if this is the primary monitor instance
     std::function<void(juce::OSCMessage msg)> messageReceived;
     void oscMessageReceived(const juce::OSCMessage& msg) override;
     juce::uint32 lastMessageTime = 0;
@@ -20,11 +20,11 @@ public:
     void update();
     void AddListener(std::function<void(juce::OSCMessage msg)> messageReceived);
     bool Send(const juce::OSCMessage& msg);
-    bool IsConnected();
+    bool isConnected();
 
     bool sendRequestToBecomeActive();
     void setActiveState(bool is_active);
-    bool IsActiveMonitor();
+    bool isActiveMonitor();
 
     bool sendMonitoringMode(int input_mode);
     bool sendMasterYPR(float yaw, float pitch, float roll);
